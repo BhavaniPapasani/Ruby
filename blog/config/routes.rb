9bebc02fa111dff3ed7dd219b17eb50990ff122f
BhavaniPapasani <lakshmi.p@lumiseek.com>
2020-10-19 14:25:06 +0530

    Third Task

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :authors do
     collection do
       get :postsbyauthor
     end
   end
   resources :posts
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
